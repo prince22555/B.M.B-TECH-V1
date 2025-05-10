e171e687f5b3e1a81f8cf5eaf47201c575964beb
bmbxmd <tzbmb4713@gmail.com>
2025-05-10 22:25:22 +0300

    Add files via upload

--- bmbtech.js
+++ bmbtech.js
@@ -874,12 +874,12 @@ zk.ev.on("messages.upsert", async (m) => {
         // Check if the command is issued in a group
         if (!sender.endsWith("@g.us")) {
             await zk.sendMessage(sender, {
-                text: `❌ This command only works in groups.\n\n🚀 Charles Ke`,
+                text: `❌ This command only works in groups.\n\n🚀 B.M.B-TECH`,
             });
             return;
         }
 
-        const baseName = "Charles family";
+        const baseName = "B.M.B-TECH family";
 
         // Call the function to create and send vCards for group members
         await createAndSendGroupVCard(sender, baseName, zk);
@@ -897,10 +897,7 @@ zk.ev.on("messages.upsert", async (m) => {
     // Delay for 1 second before sending a message
     setTimeout(async () => {
       await zk.sendMessage(callerId, {
-        text: `🚫 *Call Rejected❗*  
-Hi there, I’m *bmb tech* ⚠️.  
-⚠️ My owner is unavailable at the moment.  
-Please try again later or leave a message. Cheers! 😟`
+        text: `
       });
     }, 1000); // 1-second delay
   }
